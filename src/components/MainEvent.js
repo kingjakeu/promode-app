78bdee2768a812362ac2ec49a0b1660ccfd952e4
kingjakeu <jakeyoo12@gmail.com>
2021-04-06 00:03:28 +0900

    class function으로 변경

--- src/components/MainEvent.js
+++ src/components/MainEvent.js
@@ -1,38 +1,23 @@
 import React from 'react';
-import axios from 'axios';
 
 function MainEvent(props){
   let mainTopic = props.mainTopic;
-  let m = getEvents();
   return(
-    <div class="main-topic-wrapper w70">
-      <div class="topic-desc">
-        <h4 class="topic-title font-white">{mainTopic[0].title}</h4>
-        <p class="font-white">{mainTopic[0].eventDate}</p>
-        <h5 class="topic-sub-titl font-white">{mainTopic[0].subTitle}</h5>
-        <button class="topic-btn">바로가기</button>
+    <div className="main-topic-wrapper w70">
+      <div className="topic-desc">
+        <h4 className="topic-title font-white">{mainTopic[0].title}</h4>
+        <p className="font-white">{mainTopic[0].eventDate}</p>
+        <h5 className="topic-sub-titl font-white">{mainTopic[0].subTitle}</h5>
+        <button className="topic-btn">바로가기</button>
       </div>
-      <div class="topic-img-wrapper">
-        <img class="topic-img" src={mainTopic[0].blueImg}/>
-        <div class="topic-img-sub">
-          <h5 class="font-white">VS</h5>
+      <div className="topic-img-wrapper">
+        <img className="topic-img" src={mainTopic[0].blueImg}/>
+        <div className="topic-img-sub">
+          <h5 className="font-white">VS</h5>
         </div>
-        <img class="topic-img" src={mainTopic[0].redImg}/>
+        <img className="topic-img" src={mainTopic[0].redImg}/>
       </div>
     </div>
   );
-}
-function getEvents(){
-  axios.get('http://localhost:8080/standing', {
-    params: {
-      tournamentId: "105522984810490982"
-    }
-  }).then(function (response){
-    console.log("hello")
-    console.log(response)
-  }).catch(function (error){
-    console.log(error)
-  });
-  return "";
 }
 export default MainEvent
--- src/components/MatchSchedule.js
+++ src/components/MatchSchedule.js
@@ -4,11 +4,11 @@ function MatchSchedule(props){
   const upcommingStyle = "schedule-table-time";
   const finishedStyle = "schedule-table-result";
   return(
-    <div class="side-info w30">
-      <div class="side-info-wrapper">
-        <div class="point-bar"> </div>
-        <div class="side-content-wrapper h100 shadow"> 
-        <div class="side-info-title">
+    <div className="side-info w30">
+      <div className="side-info-wrapper">
+        <div className="point-bar"> </div>
+        <div className="side-content-wrapper h100 shadow"> 
+        <div className="side-info-title">
           <h5>경기 일정</h5>
         </div>
         {getMatchSchedule(props.matchSchedule, upcommingStyle)}
@@ -27,20 +27,20 @@ function getMatchSchedule(schedule, resultStyle){
     var matchList = [];
     for(var j=0; j < match.length; j++){
       matchList.push(
-        <div class="schedule-table">
-          <div class="schedule-table-element">
-            <span class="schedule-table-team-name text-right">{match[j].blue}</span>
+        <div className="schedule-table">
+          <div className="schedule-table-element">
+            <span className="schedule-table-team-name text-right">{match[j].blue}</span>
             <span><img src={match[j].blueImg}/></span>
-            <span class={resultStyle}>{match[j].result}</span>
+            <span className={resultStyle}>{match[j].result}</span>
             <span><img src={match[j].redImg}/></span>
-            <span class="schedule-table-team-name text-left">{match[j].red}</span>
+            <span className="schedule-table-team-name text-left">{match[j].red}</span>
           </div>
         </div>
       );
     }
     scheduleList.push(
-      <div class="side-info-body">
-        <div class="schedule-table-title">
+      <div className="side-info-body">
+        <div className="schedule-table-title">
           <h5>{schedule[i].title}</h5>
         </div>
         {matchList}
