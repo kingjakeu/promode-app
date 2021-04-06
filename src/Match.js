50b3478b0b04454e929cac39a890da9889bdb260
kingjakeu <jakeyoo12@gmail.com>
2021-04-07 00:22:23 +0900

    라우터 설정

---
+++ src/Match.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Match(){
+  return(
+    <div className="content-wrapper w100">
+      <ul className="w100">
+        <li>
+          <div className="match-list-item shadow">
+            <div className="match-team-box-score"> 
+              box
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Match;
